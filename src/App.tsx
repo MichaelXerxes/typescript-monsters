093cb0b60a56723899bf4b2ed389a8d7c658921f
MichaelXerxes <hiszpan4444@googlemail.com>
2023-01-30 07:49:06 +0000

    Typescript , react

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState,ChangeEvent } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getData } from './utils/data.utils';
+import SearchBox from './components/search-box/search-box.component';
+import CardList from './components/card-list/card-list.component';
 
-function App() {
+export type Monster={
+  id:string;
+  name:string;
+  email:string;
+};
+
+const App=()=> {
+  const[searchField,setSearchField]=useState('');
+  const[monsters,setMonsters]=useState<Monster[]>([]);
+  const [filteredMonsters,setFilteredMonsters]=useState(monsters);
+
+useEffect(()=>{
+  const fetchUsers=async()=>{
+    const users=await getData<Array<Monster>>('http://jsonplaceholder.typicode.com/users');
+    setMonsters(users);
+  };
+  fetchUsers();
+},[]);
+useEffect(()=>{
+  const newFilteredMonsters=monsters.filter((monster)=>{
+    return monster.name.toLocaleLowerCase().includes(searchField);
+  });
+  setFilteredMonsters(newFilteredMonsters);
+},[monsters,searchField]);
+
+const onSearchChange=(event:ChangeEvent<HTMLInputElement>):void=>{
+  const searchFiealdString=event.target.value.toLocaleLowerCase();
+  setSearchField(searchFiealdString);
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='app-title'>Monsters Show Cards</h1>
+
+      <SearchBox
+      
+       className='monsters-search-box'
+       onChangeHandler={onSearchChange}
+       placeholder='search monsters'
+       
+      />
+      <CardList monsters={filteredMonsters}/>
     </div>
   );
 }
---
+++ src/components/search-box/search-box.component.tsx
@@ -0,0 +1,35 @@
+import { ChangeEventHandler, ChangeEvent } from "react";
+import { Monster } from "../../App";
+import './search-box.styles.scss';
+interface ISearchBoxProps extends IChangeHandlerProps{
+    className:string;
+    placeholder?:string;
+};
+interface IChangeHandlerProps{
+    onChangeHandler:(e:object)=>void
+};
+
+type SearchBoxProps={
+    className:string;
+    placeholder:string;
+    //onChangeHandler:ChangeEventHandler<HTMLInputElement>;
+    onChangeHandler:(event:ChangeEvent<HTMLInputElement>)=>void;
+    //func:ChangeEventHandler;
+    //monster:Monster;
+    
+};
+const SearchBox=({className,placeholder,onChangeHandler}:SearchBoxProps)=>{
+    return (
+    <input 
+        className={`search-box ${className}`}
+        type='search'
+        placeholder={placeholder}
+        onChange={onChangeHandler}
+    />);
+};
+
+
+
+export default SearchBox;
+
+
